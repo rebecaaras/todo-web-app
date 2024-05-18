47baddbe03bdbfc69bfb6f37bf4e4dbde4931ef7
rebecaaras <rebecaaraujo2013@gmail.com>
2024-05-17 21:52:11 -0300

    Adiciona funções de deletar tarefa e marcar como concluída
    As funcionalidades de deletar tarefa no ícone x, marcar ou desmarcar tarefa como concluída foram adicionadas. Também foi adicionada a função de salvar as modificações no armazenamento local.

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ function appendNewTask(){
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    
     inputBox.value = '';
+    saveData();
 }
 
 function addTask(){
@@ -21,3 +27,23 @@ inputBox.addEventListener('keydown', function (e){
         addTask();
     }
 })
+
+listContainer.addEventListener("click", function (e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+
+    saveData();
+})
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
